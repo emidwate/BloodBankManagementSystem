759278b256d9fe2b16b8d9557179ae362e9501ed
emidwate <matthewcode04@gmail.com>
2024-06-16 16:48:25 +0200

    Changed names for consistency

--- data visualization/database_connection.py
+++ data visualization/database_connection.py
@@ -10,19 +10,19 @@ def update_data(): # wrapped in a function so that the data is updated whenever 
 
   cursor = db.cursor()
 
-  # cursor.execute("""SELECT hospital_name, count(transfusion_id) as total_count
-  #               FROM blood_transfusion bt
-  #               INNER JOIN hospital h
-  #               ON bt.hospital_id = h.hospital_id
-  #               GROUP BY h.hospital_name
-  #               # ORDER BY count(transfusion_id);""") 
-
-  cursor.execute("""SELECT facility_name, count(transfusion_id) as number_of_transfusions
+  cursor.execute("""SELECT hospital_name, count(transfusion_id) as counter
                 FROM blood_transfusion bt
-                INNER JOIN private_facility pf
-                ON bt.private_facility_id = pf.facility_id
-                GROUP BY pf.facility_name
-                ORDER BY count(transfusion_id);""")
+                INNER JOIN hospital h
+                ON bt.hospital_id = h.hospital_id
+                GROUP BY h.hospital_name
+                # ORDER BY count(transfusion_id);""") 
+
+  # cursor.execute("""SELECT facility_name, count(transfusion_id) as counter
+  #               FROM blood_transfusion bt
+  #               INNER JOIN private_facility pf
+  #               ON bt.private_facility_id = pf.facility_id
+  #               GROUP BY pf.facility_name
+  #               ORDER BY count(transfusion_id);""")
 
   # You can use this commented query also to see transfusions that have taken place in private facilities, just remember to comment on the above query after after you delete the comment from this one
 
