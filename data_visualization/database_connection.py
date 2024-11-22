ea19b8d8607c33cef571602f889141afa70d5048
emidwate <matthewcode04@gmail.com>
2024-11-22 17:07:22 +0100

    Removed excessive query, spelling changes

--- data_visualization/database_connection.py
+++ data_visualization/database_connection.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
-def update_data(): # wrapped in a function so that the data is updated whenever the database changes
+def update_data():                            # wrapped in a function so that the data is updated whenever the database changes
   db = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "password", # enter here password you set for the root user on your computer
+    password = "password",                    # enter here password you set for the mysql root user 
     database = "bbms"
   )
 
@@ -16,15 +16,6 @@ def update_data(): # wrapped in a function so that the data is updated whenever 
                 ON bt.hospital_id = h.hospital_id
                 GROUP BY h.hospital_name
                 ORDER BY count(transfusion_id);""") 
-
-  # cursor.execute("""SELECT facility_name, count(transfusion_id) as counter
-  #               FROM blood_transfusion bt
-  #               INNER JOIN private_facility pf
-  #               ON bt.private_facility_id = pf.facility_id
-  #               GROUP BY pf.facility_name
-  #               ORDER BY count(transfusion_id);""")
-
-  # You can use this commented query also to see transfusions that have taken place in private facilities, just remember to comment on the above query after after you delete the comment from this one
 
   result = cursor.fetchall()
 
